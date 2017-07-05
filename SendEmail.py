9449c1cc9a2144217912ac4db15d80769f0ce24d
bh69399 <402323099@qq.com>
2017-07-05 10:19:05 +0800

    发送邮件

---
+++ SendEmail.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "402323099@qq.com"
+gmail_pwd = "qvpzlxnparffcahc"
+
+
+def mail(to, subject, text, attach):
+    msg = MIMEMultipart()
+
+    msg['From'] = gmail_user
+    msg['To'] = to
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(open(attach, 'rb').read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition',
+                    'attachment; filename="%s"' % os.path.basename(attach))
+    msg.attach(part)
+
+    mailServer = smtplib.SMTP("smtp.qq.com", 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmail_user, gmail_pwd)
+    mailServer.sendmail(gmail_user, to, msg.as_string())
+    mailServer.close()
+
+
+try:
+    to = "402323099@qq.com"
+    subject = "from python"
+    text = "测试"
+    attach = "D:/python/ArcpyBook/data/bc_pop1996.csv"
+    mail(to, subject, text, attach)
+    print "已发送至" + to
+
+except Exception as e:
+    print e
